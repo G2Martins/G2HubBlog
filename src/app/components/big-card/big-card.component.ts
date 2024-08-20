b5e6a7a7f50f3d5164bbf8925898f52340508d60
G2Martins <gustavogripaldi@outlook.com>
2024-08-20 03:07:30 -0300

    alter: news dunamic changes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-big-card',
-  templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+	selector: 'app-big-card',
+	templateUrl: './big-card.component.html',
+	styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+	@Input()
+	photoCover: string=""
 
-  constructor() { }
+	@Input()
+	cardTitle: string=""
+	
+	@Input()
+	cardDescription: string=""
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
